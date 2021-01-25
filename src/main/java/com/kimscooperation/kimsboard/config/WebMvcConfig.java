5d0ade120ea2d5344bb5428be2e5b08328b5ee28
zxc7023 <zxc7023@gmail.com>
2021-01-25 22:05:04 +0900

    [fix] swagger-ui 오류 수정

--- src/main/java/com/kimscooperation/kimsboard/config/WebMvcConfig.java
+++ src/main/java/com/kimscooperation/kimsboard/config/WebMvcConfig.java
@@ -1,30 +1,33 @@
 
-// package com.kimscooperation.kimsboard.config;
+package com.kimscooperation.kimsboard.config;
 
-// import java.io.IOException;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.core.io.ClassPathResource;
-// import org.springframework.core.io.Resource;
-// import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// import org.springframework.web.servlet.resource.PathResourceResolver;
+import java.io.IOException;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
-// @Configuration
-// public class WebMvcConfig implements WebMvcConfigurer {
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
 
-//     @Override
-//     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//       registry.addResourceHandler("/**")
-//         .addResourceLocations("classpath:/static/")
-//         .resourceChain(true)
-//         .addResolver(new PathResourceResolver() {
-//             @Override
-//             protected Resource getResource(String resourcePath,
-//                 Resource location) throws IOException {
-//                 Resource requestedResource = location.createRelative(resourcePath);
-//                 return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-//                 : new ClassPathResource("/static/index.html");
-//             }
-//         });
-//     }
-// }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    //   registry.addResourceHandler("/**")
+    //     .addResourceLocations("classpath:/static/")
+    //     .resourceChain(true)
+    //     .addResolver(new PathResourceResolver() {
+    //         @Override
+    //         protected Resource getResource(String resourcePath,
+    //             Resource location) throws IOException {
+    //             Resource requestedResource = location.createRelative(resourcePath);
+    //             return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+    //             : new ClassPathResource("/static/index.html");
+    //         }
+    //     });
+    }
+}
