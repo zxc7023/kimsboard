32e81a96efd544599b029e463f2dccc17e7222ae
zxc7023 <zxc7023@gmail.com>
2021-01-25 22:00:06 +0900

    [fix] 오라클 커넥션 이슈 조치

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -38,4 +38,4 @@ echo "> $JAR_NAME 실행"
 #  이 이슈를 해결하기 위해 nohub.out파일을 표준 입출력용으로 별도로 사용합니다.
 #  이렇게 하지 않으면 nohup.out파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력됩니다.
 #  nohub이 끝나기 전까지 CodeDeploy도 끝나지 않으니 꼭 이렇게 해야합니다.
-nohup java -jar -Dspring.profiles.active=production $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Djava.security.egd=file:///dev/urandom -Dspring.profiles.active=production $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
